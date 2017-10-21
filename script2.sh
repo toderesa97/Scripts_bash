025a6c82a05637553553915e48c20cdc39f90e8b
Tomás Denis Reyes Sánchez <toderesa97@gmail.com>
2017-10-21 22:44:11 +0100

    Script 2
    This script retrieves all the users in /etc/passwd whose UID are within a given range. If no range is given the maximum and minimum is grabbed from /etc/login.defs file.

---
+++ script2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $# -le 0 ]; then
+	min=$(grep -E "UID_MIN" /etc/login.defs | sed -n 1,1p | tr -s ' ' | cut -f2 -d' ')
+	max=$(grep -E "UID_MAX" /etc/login.defs | sed -n 1,1p | tr -s ' ' | cut -f2 -d' ')
+else
+	if [ $# -le 1 ]; then
+		echo "Fatal error: Expected 2 arguments"
+		exit 1
+	else	
+		min=$1
+		max=$2
+		if [ $min -gt $max ]; then
+			echo "Fatal error: first param must be lower than second"
+			exit 1
+		fi
+	fi
+fi
+p="p"
+# Finding all users whose UID are within the given range
+lines=$(wc -l /etc/passwd | cut -f1 -d' ')
+echo "LINES ---> $lines;  min $min and max $max"
+for i in `seq 1 $lines`;
+do
+	#echo "single line--> $line"
+	user=$(sed -n $i,$i$p /etc/passwd | cut -f1 -d:)
+	uid=$(sed -n $i,$i$p /etc/passwd | cut -f3 -d:)
+	if [ $uid -ge $min ] && [ $uid -le $max ]; then
+		echo $user
+	fi
+done
