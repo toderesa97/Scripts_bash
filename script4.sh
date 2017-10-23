8258eb4e374b1670d58ed66055c10ce5be1ae2a3
Tomás Denis Reyes Sánchez <toderesa97@gmail.com>
2017-10-23 09:13:22 +0100

    Upate script4: renaming files (working version)

--- script4.sh
+++ script4.sh
@@ -3,29 +3,42 @@
 if [ -f files_cpp ]; then
 	rm files_cpp
 fi
+if [ -f tmp ]; then
+	rm tmp
+fi
+if [ -f tmp1 ]; then
+	rm tmp1
+fi
 
+# searching files which meet the condition
 if [ $# -le 0 ]; then
 	find ./ -iname "*.cpp" > files_cpp
-	directory="./"
 else
 	find $1 -iname "*.cpp" > files_cpp
-	directory="$1"
 fi
 
 p="p"
-lines=$(wc -l files_cpp | cut -f1 -d' ')
+lines=$(wc -l files_cpp | cut -f1 -d' ') 
 for i in `seq 1 $lines`; do
-	sed -n $i,$i$p files_cpp > tmp
+	sed -n $i,$i$p files_cpp > tmp   	
 	line=$(sed -n $i,$i$p files_cpp)
-	cut -f3 -d'/' tmp > tmp1
+	route_until=$(($(grep -o "/" tmp | wc -l)+1))
+	cut -f$route_until- -d'/' tmp > tmp1
 	file_name=$(cut -f1 -d. tmp1)
 	sed -n $i,$i$p files_cpp > tmp
 	route_until=$(grep -o "/" tmp | wc -l)
 	route=$(cut -f1-$route_until -d/ tmp)
-	echo "file_name = $file_name"
-	echo "route--> $route"
-	echo "mv $line $route/$file_name.cp"
+	mv $line $route/$file_name.cp
 done
-# rm files_cpp
+# dropping safely created files
+if [ -f files_cpp ]; then
+	rm files_cpp
+fi
+if [ -f tmp ]; then
+	rm tmp
+fi
+if [ -f tmp1 ]; then
+	rm tmp1
+fi
 
 
